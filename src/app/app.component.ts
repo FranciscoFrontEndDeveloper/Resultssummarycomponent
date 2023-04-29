0942e12f309634313eb7f787ec5eb2e74941f3bc
FRANCISCO <pachonetworking@gmail.com>
2023-04-28 23:43:14 -0500

    normalize installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'Resultssummarycomponent';
+  title = 'Frontend Mentor | Results summary component';
 }
